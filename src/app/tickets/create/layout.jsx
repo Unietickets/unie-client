1a5f52687525781de37497d503f52a037087239a
Amir <far20022002213@gmail.com>
2025-02-16 19:40:40 +0500

    feature: added Create Ticket page in app

---
+++ src/app/tickets/create/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from '@shared/ui';
+
+const CreateTicketLayout = ({ children }) => {
+  return (
+    <Container>
+      {children}
+    </Container>
+  );
+};
+
+export default CreateTicketLayout;
