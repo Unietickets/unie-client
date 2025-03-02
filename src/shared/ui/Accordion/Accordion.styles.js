87b946b80f300904da0e36c76d1c1e07cb031c22
Amir <far20022002213@gmail.com>
2025-03-02 05:54:50 +0500

    fix: updated gradient in accordion

--- src/shared/ui/Accordion/Accordion.styles.js
+++ src/shared/ui/Accordion/Accordion.styles.js
@@ -3,9 +3,10 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background: linear-gradient(0deg, #343434, #343434),
+  background:
     radial-gradient(73.26% 73.26% at -3.05% 107.59%, rgba(220, 194, 54, 0.2) 0%, rgba(50, 50, 50, 0.2) 100%),
-    radial-gradient(68.02% 68.02% at 100% 104.46%, rgba(105, 245, 196, 0) 0%, rgba(50, 50, 50, 0.2) 100%);
+    radial-gradient(68.02% 68.02% at 100% 104.46%, rgba(105, 245, 196, 0) 0%, rgba(50, 50, 50, 0.2) 100%),
+    linear-gradient(0deg, rgba(52, 52, 52, 0.9), rgba(52, 52, 52, 0.9));
 
   border-radius: 15px;
   padding: 12px 24px;
