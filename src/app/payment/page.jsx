2f52dc269b9d75b801a8e17406fe913b955fa154
Amir <far20022002213@gmail.com>
2025-06-10 17:20:36 +0500

    fix: disabled static gen for payment pages

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import PaymentPage from '@/views/payment';
 
+// Отключаем статическую генерацию для этой страницы
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
 const page = () => {
   return (
     <PaymentPage />
--- src/app/payment/success/page.jsx
+++ src/app/payment/success/page.jsx
@@ -1,5 +1,9 @@
 import SuccessPaymentPage from '@/views/payment/success';
 
+// Отключаем статическую генерацию для этой страницы
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
 async function Page() {
   return (
     <SuccessPaymentPage />
